7369714854587db67f429559063ac2ac76aeb691
Tacconinja007 <hallobecher@gmail.com>
2022-05-23 13:24:37 +0200

    semicolon, spaces after colons

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Dont use any here, this is just to disable the warning of implicit any type
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
@@ -15,4 +15,4 @@ const server = app.listen(80, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(`Listening on ${host}:${port}`);
-})
+});
