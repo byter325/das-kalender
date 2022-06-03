571e2e731deea9c375896909546dc3e4b6000f49
Tacconinja007 <hallobecher@gmail.com>
2022-06-03 21:58:10 +0200

    enabling beuatiful bootstrap tooltips

---
+++ app/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function initTooltips() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+};
+
+window.onload = () => {
+    initTooltips();
+};
