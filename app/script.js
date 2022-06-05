6ceb2175dba31c2f97c24e543a1695020fffe38e
kim-gabriel.deckert <kim-gabriel.deckert@kit.edu>
2022-06-05 12:55:11 +0200

    AJAX

--- app/script.js
+++ app/script.js
@@ -2,11 +2,20 @@
 
 function initTooltips() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     });
 };
 
-window.onload = () => {
+function insertEvents(course, from, to) {
+    $.ajax({
+        url: `/api/getRaplaEvents/${course}?from=${from}&to=${to}`
+    }).done(function () {
+        $('#eventInsert').append(data);
+    });;
+}
+
+$(() => {
     initTooltips();
-};
+    insertEvents("TINF21B1", "2022-06-06", "2022-06-12");
+});
