c592ac6bd6e330284ebd5593e9b2f785ab7bec3f
kim-gabriel.deckert <kim-gabriel.deckert@kit.edu>
2022-06-08 19:04:17 +0200

    changes

--- app/script.js
+++ app/script.js
@@ -13,10 +13,56 @@ function insertEvents(course, from, to) {
         xhrFields: { withCredentials: true }
     }).done(function (data) {
         $('#eventInsert').append(data);
-    });;
+        adjustDays();
+    });
+}
+
+function adjustDays() {
+    var days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+    var dayCols = [2, 6, 10, 14, 18, 22, 26];
+    days.forEach((day, dayindex) => {
+        for (let i = 0; i < 44; i++) {
+            var eventsAtRow = [];
+            $('[data-day=' + day + ']').each((index, element) => {
+                var rowStart = Math.floor(((parseInt($(element).attr("data-starthour")) - 8) * 60 + parseInt($(element).attr("data-startminute"))) / 15);
+                var rowEnd = Math.floor(((parseInt($(element).attr("data-starthour")) - 8) * 60 + parseInt($(element).attr("data-startminute")) + parseInt($(element).attr("data-duration"))) / 15);
+                if (rowStart <= i && rowEnd >= i) eventsAtRow.push(element);
+            });
+            console.log(eventsAtRow.length);
+            if (eventsAtRow.length == 2) {
+                eventsAtRow.forEach((ki) => {
+                    $(ki).removeClass("ki-colspan-4");
+                    $(ki).addClass("ki-colspan-2");
+                });
+                $(eventsAtRow[1]).removeClass('ki-day-' + day);
+                $(eventsAtRow[1]).addClass('ki-col-' + (dayCols[dayindex] + 2));
+            }
+            if (eventsAtRow.length == 3) {
+                eventsAtRow.forEach((ki) => {
+                    $(ki).removeClass("ki-colspan-4");
+                    $(ki).addClass("ki-colspan-1");
+                });
+                $(eventsAtRow[1]).removeClass('ki-day-' + day);
+                $(eventsAtRow[1]).addClass('ki-col-' + (dayCols[dayindex] + 1));
+                $(eventsAtRow[2]).removeClass('ki-day-' + day);
+                $(eventsAtRow[2]).addClass('ki-col-' + (dayCols[dayindex] + 2));
+            }
+            if (eventsAtRow.length >= 4) {
+                eventsAtRow.forEach((ki) => {
+                    $(ki).removeClass("ki-colspan-4");
+                    $(ki).addClass("ki-colspan-1");
+                });
+                $(eventsAtRow[1]).removeClass('ki-day-' + day);
+                $(eventsAtRow[1]).addClass('ki-col-' + (dayCols[dayindex] + 1));
+                $(eventsAtRow[2]).removeClass('ki-day-' + day);
+                $(eventsAtRow[2]).addClass('ki-col-' + (dayCols[dayindex] + 2));
+                $(eventsAtRow[3]).removeClass('ki-day-' + day);
+                $(eventsAtRow[3]).addClass('ki-col-' + (dayCols[dayindex] + 3));
+            }
+        }
+    });
 }
 
 $(() => {
-    initTooltips();
     insertEvents("TINF21B1", "2022-06-06", "2022-06-12");
 });
