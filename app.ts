52e17b35ba0c75c7d033e708593b29f2a9ce2fbc
Benjamin Appel <benjamin.appel@sap.com>
2022-06-29 12:25:55 +0200

    fixed small Issue with the security folder

--- app.ts
+++ app.ts
@@ -19,12 +19,16 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const app: Application = express();
 const port = 8080;
+const securityPath = path.join(__dirname, "security");
+
+if(!fs.existsSync(securityPath)) {
+    throw new Error("Security folder not found. Did you forget to add the folder and the certificate?");
+}
 
 const options = {
     key: fs.readFileSync(path.join(__dirname, "security", "server.key")),
     cert: fs.readFileSync(path.join(__dirname, "security", "server.cert"))
 };
-
 
 const server: Server = https.createServer(options, app).listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
