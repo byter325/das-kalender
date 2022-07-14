1d89fcc70e96e18263b9bd252cdf8bb0cf63eeb0
Tacconinja007 <hallobecher@gmail.com>
2022-07-14 14:27:53 +0200

    function setCookie restored

--- app/script.js
+++ app/script.js
@@ -17,6 +17,13 @@ function getCookie(cname) {
     return "";
 }
 
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function getCurrentKw() {
     var date = new Date();
     date.setHours(0, 0, 0, 0);
